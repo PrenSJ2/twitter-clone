97ea56be8082fa69d5d6dc5e73176ea801b1ae6f
PrenSJ2 <seb.prentice@hotmail.com>
2022-06-04 22:07:55 +0100

    inital commit

---
+++ components/Widgets.tsx
@@ -0,0 +1,18 @@
+import { SearchCircleIcon } from '@heroicons/react/outline'
+import React from 'react'
+
+function Widgets() {
+  return (
+    <div className='mt-2 px-2'>
+        {/* Search */}
+        <div className='flex items-center space-x-2 bg-gray-100 p-3 rounded-full'>
+            <SearchCircleIcon className='h-5 w-5 text-gray-400'/>
+            <input type="text" placeholder='Search Twitter' className='bg-transparent flex-1 outline-none'/>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Widgets
